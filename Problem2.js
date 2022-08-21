6456fef7ac27f24e798b24eb8d9e144f32307a31
abusalehrony <abusalehrony12@gmail.com>
2022-08-21 16:44:32 +0600

    after submission

--- Problem2.js
+++ Problem2.js
@@ -5,12 +5,12 @@ function isJavaScriptFile(fileName) {
     if (isString !== "string") {
         return "Please enter string input!";
     }
-    else if (isString === "string" && fileName.includes(".js")) {
+    else if (isString === "string" && fileName.endsWith(".js")) {
         return true;
     }
     else {
         return false;
     }
 }
-console.log(isJavaScriptFile(2));
+console.log(isJavaScriptFile("tree.js.jpg"));
 
--- Problem4.js
+++ Problem4.js
@@ -1,31 +1,23 @@
 // Problem 4: publicBusFare
 
 function publicBusFare(totalPeople) {
-
     const isNumber = typeof totalPeople;
     const perBusCapacity = 50;
     const permicroBusCapacity = 11;
     const publicBusFare = 250;
-
     if (isNumber === "number" && totalPeople > 0) {
-        if (totalPeople >= perBusCapacity) {
-            const restPassenger = totalPeople % perBusCapacity;
-            if (restPassenger >= permicroBusCapacity) {
-                const otherPassenger = restPassenger % permicroBusCapacity;
-                return otherPassenger * publicBusFare;
-            }
-        }
-        else if (totalPeople >= permicroBusCapacity && totalPeople < perBusCapacity) {
-            const restPassenger = totalPeople % permicroBusCapacity;
-            return restPassenger * publicBusFare;
-        }
-        else {
-            return totalPeople * publicBusFare;
-        }
+        const restPassenger = totalPeople % perBusCapacity;
+        const otherPassenger = restPassenger % permicroBusCapacity;
+        return otherPassenger * publicBusFare;
     }
     else {
         return "Please enter a valid Input Number";
     }
 }
 
+console.log(publicBusFare(50));
+console.log(publicBusFare(55));
+console.log(publicBusFare(112));
 console.log(publicBusFare(235));
+console.log(publicBusFare(365));
+
